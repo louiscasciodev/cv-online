7816dd25840cd37a5fcc03fbf381b2cfe5f1ac0b
dolfant <louis.cascio@hotmail.fr>
2019-12-27 18:46:56 +0100

    feat(auth): reset App

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, SignIn } from './components/user/pages'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100) // fake async
-  }
-}
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    fakeAuth.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to='/sign-in' />
-  )} />
-)
 
 const louisTheme = createMuiTheme({
   palette: {
@@ -49,8 +28,7 @@ export default () => {
       <Router>
         <Switch>
           <Route exact path="/sign-in" component={SignIn} />
-          {/* <Route exact path="/home" component={Home} /> */}
-          <PrivateRoute path='/home' component={Home} />
+          <Route exact path="/home" component={Home} />
         </Switch>
       </Router>
     </ThemeProvider>
