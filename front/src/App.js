0799e10a3501a79499f63c1246c70441a030e338
dolfant <louis.cascio@hotmail.fr>
2019-12-27 19:22:10 +0100

    feat(home): auth required

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,10 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router, Switch, Route, Redirect, useHistory, useLocation, useRouteMatch,
+} from "react-router-dom";
 import { Home, SignIn } from './components/user/pages'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100) // fake async
-  }
-}
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    fakeAuth.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to='/sign-in' />
-  )} />
-)
 
 const louisTheme = createMuiTheme({
   palette: {
@@ -43,14 +24,24 @@ const louisTheme = createMuiTheme({
 });
 
 export default () => {
+
+  const [isAuthenticated, setisAuthenticated] = useState(true);
+
   return (
     <ThemeProvider theme={louisTheme}>
       <CssBaseline />
       <Router>
         <Switch>
           <Route exact path="/sign-in" component={SignIn} />
-          {/* <Route exact path="/home" component={Home} /> */}
-          <PrivateRoute path='/home' component={Home} />
+          {isAuthenticated ? (
+            <Route exact path="/home" component={Home} />
+          ) : (
+              <Redirect
+                to={{
+                  pathname: "/sign-in",
+                }}
+              />
+            )}
         </Switch>
       </Router>
     </ThemeProvider>
